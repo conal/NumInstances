6a4a24ae24533b31d0140115e8259fc5fba14e28
Daniel Wagner <daniel@wagner-home.com>
2013-04-24 14:48:34 -0400

    only define incomplete instances for functions in old GHCs that require them

--- src/Data/NumInstances/Function.hs
+++ src/Data/NumInstances/Function.hs
@@ -15,24 +15,7 @@ module Data.NumInstances.Function () where
 
 import Control.Applicative
 
-import Data.NumInstances.Util
-
-noFun :: String -> a
-noFun = noOv "function"
-
--- Eq & Show are prerequisites for Num, so they need to be faked here
-instance Eq (a->b) where
-  (==) = noFun "(==)"
-  (/=) = noFun "(/=)"
-
-instance Ord b => Ord (a->b) where
-  min = liftA2 min
-  max = liftA2 max
-
-instance Show (a->b) where
-  show      = noFun "show"
-  showsPrec = noFun "showsPrec"
-  showList  = noFun "showList"
+import Data.NumInstances.PreRequisites () -- Eq, Ord, Show, if necessary
 
 instance Num b => Num (a->b) where
   negate      = fmap negate
